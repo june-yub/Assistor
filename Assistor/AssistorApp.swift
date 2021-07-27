f82fae065cd92ea209ef0659322bd40dc91e706b
jeonjunyeop <john40644064@gmail.com>
2021-07-27 09:08:17 +0900

    Initial Commit

---
+++ Assistor/AssistorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssistorApp.swift
+//  Assistor
+//
+//  Created by Junyeop Jeon on 7/27/21.
+//
+
+import SwiftUI
+
+@main
+struct AssistorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
